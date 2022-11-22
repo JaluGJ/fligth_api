a55bd7ba8afad35fa1ebb6b2da934ec023769a4e
JaluGJ <gabriel.i.jalil@gmail.com>
2022-11-22 19:25:43 -0300

    feat(airportsSeed): add airports seeder

---
+++ src/utils/airportSeed.js
@@ -0,0 +1,33 @@
+const fs = require("fs");
+
+function airportSeeder() {
+	csv = fs.readFileSync("../../archive/airports.csv")
+	let array = csv.toString().split("\n")
+	let data = []
+  let header = array[0].split(",")
+
+	array.forEach((el, i) => {
+		if (i === 0) return
+		if (!el) return
+    let e = el.split(",")
+
+		info = { 
+      IATA_CODE: e[0],
+      AIRPORT: e[1],
+      CITY: e[2],
+      STATE: e[3],
+      COUNTRY: e[4],
+      LATITUDE: e[5],
+      LONGITUDE: e[6], 
+    }
+		return data.push(info)
+	})
+	return data
+}
+
+
+// airportSeeder()
+
+module.exports = {
+	airportSeeder
+}
