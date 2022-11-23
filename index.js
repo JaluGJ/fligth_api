773bfea4e0779b1bd32a95dd9a0e8842e730720f
JaluGJ <gabriel.i.jalil@gmail.com>
2022-11-22 21:22:35 -0300

    feat(index): add seeder ejecution

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 require("dotenv").config()
 const app = require("./src/app")
-const { db } = require("./src/db")
+const { db, Airline, Airport, Flight } = require("./src/db")
+const { airlineSeeder } = require("./src/utils/airlineSeed")
+const { airportSeeder } = require("./src/utils/airportSeed")
 const { flightSeeder } = require("./src/utils/flightSeed")
 const { PORT } = process.env
 
-console.log(flightSeeder())
+// flightSeeder()
+
+function airports() {
+    const airports = airportSeeder()
+    airports.forEach(async (el) => {
+        const [newAirport, isCreated] = await Airport.findOrCreate({
+            where: {
+                IATA_CODE: el.IATA_CODE
+            },
+            defaults: {
+                IATA_CODE: el.IATA_CODE,
+                AIRPORT: el.AIRPORT,
+                CITY: el.CITY,
+                STATE: el.STATE,
+                COUNTRY: el.COUNTRY,
+                LATITUDE: el.LATITUDE,
+                LONGITUDE: el.LONGITUDE
+            }
+        })
+        if (isCreated) console.log("already created")
+    })
+    return
+}
+
+
+function airlines() {
+    const airlines = airlineSeeder()
+    airlines.forEach(async (el) => {
+        const [newAirline, isCreated] = await Airline.findOrCreate({
+            where: {
+                IATA_CODE: el.IATA_CODE
+            },
+            defaults: {
+                IATA_CODE: el.IATA_CODE,
+                AIRLINE: el.AIRLINE
+            }
+        })
+        if (isCreated) console.log("already created")
+    })
+    return
+}
 
 let force = true
 
 db.sync({ force: force }).then(
     async () => {
         app.listen(PORT, () => {
+            airlines()
+            airports()
             console.log(`listening on port ${PORT}`)
         })
     })
