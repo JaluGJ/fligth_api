cd6d7a45dd5234ea0ff677d8c1ffc3a51657a3e6
JaluGJ <gabriel.i.jalil@gmail.com>
2022-11-24 07:00:31 -0300

    fix(/post_airport): add Bad Request on post

--- src/controller/airports/post_airport_controller.js
+++ src/controller/airports/post_airport_controller.js
@@ -3,12 +3,13 @@ const { Airport } = require("../../db")
 module.exports = {
   createNewAirport: async (req, res) => {
     try {
-      const { IATA_CODE, AIRPORT, CITY, STATE, COUNTRY, LATITUDE,  LONGITUDE} = req.body
+      const { IATA_CODE, AIRPORT, CITY, STATE, COUNTRY, LATITUDE, LONGITUDE } = req.body
+      if (!IATA_CODE || !AIRPORT || !CITY || STATE || !COUNTRY || !LATITUDE || !LONGITUDE) return res.status(401).json({ message: "Missing primary information" })
       const [newAirport, isCreated] = await Airport.findOrCreate({
-        where:{
+        where: {
           IATA_CODE
         },
-        defaults:{
+        defaults: {
           IATA_CODE,
           AIRPORT,
           CITY,
@@ -18,10 +19,10 @@ module.exports = {
           LONGITUDE
         }
       })
-      if (!isCreated) return res.status(401).json({message: "Airport already exist"})
-      return res.json({message: "New Airport created Succesfully", data: newAirport})
-  } catch(error) {
-    return res.json(400).json(error)
+      if (!isCreated) return res.status(401).json({ message: "Airport already exist" })
+      return res.json({ message: "New Airport created Succesfully", data: newAirport })
+    } catch (error) {
+      return res.json(400).json(error)
+    }
   }
-}
 }
