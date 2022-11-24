a6db730132653a2c2e3309ac46f3b0c45fa0c59d
JaluGJ <gabriel.i.jalil@gmail.com>
2022-11-24 07:16:10 -0300

    fix(/post_fligths): add Bad Request on Post

--- src/controller/flights/post_flights_controller.js
+++ src/controller/flights/post_flights_controller.js
@@ -36,50 +36,83 @@ module.exports = {
         AIRLINE_DELAY,
         LATE_AIRCRAFT_DELAY,
         WEATHER_DELAY } = req.body
-  
+
+      if (
+        YEAR
+        || MONTH
+        || DAY
+        || DAY_OF_WEEK
+        || AIRLINE
+        || FLIGHT_NUMBER
+        || TAIL_NUMBER
+        || ORIGIN_AIRPORT
+        || DESTINATION_AIRPORT
+        || SCHEDULED_DEPARTURE
+        || DEPARTURE_TIME
+        || DEPARTURE_DELAY
+        || TAXI_OUT
+        || WHEELS_OFF
+        || SCHEDULED_TIME
+        || ELAPSED_TIME
+        || AIR_TIME
+        || DISTANCE
+        || WHEELS_ON
+        || TAXI_IN
+        || SCHEDULED_ARRIVAL
+        || ARRIVAL_TIME
+        || ARRIVAL_DELAY
+        || DIVERTED
+        || CANCELLED
+        || CANCELLATION_REASON
+        || AIR_SYSTEM_DELAY
+        || SECURITY_DELAY
+        || AIRLINE_DELAY
+        || LATE_AIRCRAFT_DELAY
+        || WEATHER_DELAY) return res.status(401).json({ message: "Missing primary information" })
+
       const newFlight = await Flight.create({
-          YEAR,
-          MONTH,
-          DAY,
-          DAY_OF_WEEK,
-          FLIGHT_NUMBER,
-          TAIL_NUMBER,
-          SCHEDULED_DEPARTURE,
-          DEPARTURE_TIME,
-          DEPARTURE_DELAY,
-          TAXI_OUT,
-          WHEELS_OFF,
-          SCHEDULED_TIME,
-          ELAPSED_TIME,
-          AIR_TIME,
-          DISTANCE,
-          WHEELS_ON,
-          TAXI_IN,
-          SCHEDULED_ARRIVAL,
-          ARRIVAL_TIME,
-          ARRIVAL_DELAY,
-          DIVERTED,
-          CANCELLED,
-          CANCELLATION_REASON,
-          AIR_SYSTEM_DELAY,
-          SECURITY_DELAY,
-          AIRLINE_DELAY,
-          LATE_AIRCRAFT_DELAY,
-          WEATHER_DELAY
+        YEAR,
+        MONTH,
+        DAY,
+        DAY_OF_WEEK,
+        FLIGHT_NUMBER,
+        TAIL_NUMBER,
+        SCHEDULED_DEPARTURE,
+        DEPARTURE_TIME,
+        DEPARTURE_DELAY,
+        TAXI_OUT,
+        WHEELS_OFF,
+        SCHEDULED_TIME,
+        ELAPSED_TIME,
+        AIR_TIME,
+        DISTANCE,
+        WHEELS_ON,
+        TAXI_IN,
+        SCHEDULED_ARRIVAL,
+        ARRIVAL_TIME,
+        ARRIVAL_DELAY,
+        DIVERTED,
+        CANCELLED,
+        CANCELLATION_REASON,
+        AIR_SYSTEM_DELAY,
+        SECURITY_DELAY,
+        AIRLINE_DELAY,
+        LATE_AIRCRAFT_DELAY,
+        WEATHER_DELAY
       })
 
       let origin = await Airport.findByPk(ORIGIN_AIRPORT)
       await newFlight.setAirport(origin)
-      
+
       let destination = await Airport.findByPk(DESTINATION_AIRPORT)
       await newFlight.setAirport(destination)
-      
+
       let airline = await Airline.findByPk(AIRLINE)
       await newFlight.screAirline(airline)
-      
 
-      return res.json({data: newFlight})
-      
+
+      return res.json({ data: newFlight })
+
     } catch (error) {
       return res.status(400).json(error)
     }
